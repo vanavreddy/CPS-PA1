36fe47c2c2a8397ce6e2f498ecd917867a59e2a5
vanavreddy <vana_reddy@rediffmail.com>
2019-09-26 22:18:24 -0400

    put in loop

--- feature_extractor.py
+++ feature_extractor.py
@@ -13,49 +13,73 @@
 import csv
 import pandas as pd
 from datetime import datetime
+import numpy as np
+import collections
 
 #filename = "data/G5NZCJ017647206-Kay-left-hand_wash-soap-2019-09-21-07-12-36.wada"
 filename = "csv-data/G5NZCJ017647206-Kay-left-hand_wash-soap-2019-09-21-07-12-36.csv"
 
-f = open('./sample-features.csv', 'w')
+#f = open('./sample-features.csv', 'w')
 
-with open(filename) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    print(csv_reader)
-    counter = 0
-    for row in csv_reader:
-        #convert millisecond timestamp to seconds
-        tsecs = float(row[0].strip())/1000.0
-        #convert timestamp to dat/time format
-        print(datetime.fromtimestamp(tsecs).strftime('%m:%d:%y %H:%M:%S'))
-        #print(datetime.fromtimestamp(ts).strftime('%S'))
-        f.write(datetime.fromtimestamp(tsecs).strftime('%m:%d:%y %H:%M:%S') + '\n')
-        counter = counter + 1
-        '''
-        if(counter == 0):
-            prev = datetime.fromtimestamp(ts).strftime('%S')
-            current = datetime.fromtimestamp(ts).strftime('%S')
+def read_csv_file(filename):
+    indices = []
+    counts = []
+    csv_reader = []
+    data = []
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
         
-        current = datetime.fromtimestamp(ts).strftime('%S')
-        
-        if(current != prev):
-            print("Changed {} to {}".format(prev, current))
-            prev = current
+        print(csv_reader)
+        counter = 0
+        prev = 0
+        current = 0
+        secs = []
+        for row in csv_reader:
+            #convert millisecond timestamp to seconds
+            tsecs = float(row[0].strip())/1000.0
             
-        counter = counter + 1
-        '''
-        #print("Count :", counter)
-'''     
-   
-df = pd.read_csv(filename)
-print(df)
-print (len(df))
-
-for i in range(0,len(df),1000):
-    print (i)
-    mean_ = df.iloc[i:i+1000].mean()
-    st_dev = df.iloc[i:i+1000].std()
-    print ("index_1 {} index_2 {} mean {} stdev {}".format(i, i+1000, mean_, st_dev))
-    print(' ')
+            ts = datetime.fromtimestamp(tsecs).strftime('%S')
+            
+            secs.append(ts)
+            counter = counter + 1
+            
+        #print(secs)   
+        u, indices, counts = np.unique(secs, return_index=True, return_counts=True)
+        return indices, counts
     
-'''
+def write_features(filename, indices, counts):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        # get all the rows as a list
+        data = list(reader)
+        # transform data into numpy array
+        data = np.array(data).astype(float)
+     
+    print(data)
+    
+    f = open('features.csv', 'w+')
+    f.write("mean_x,std_x,mean_y,std_y,mean_z,std_z")
+    f.write('\n')
+    
+    for i in range(0,len(counts)):
+        
+        mean_x = np.mean(data[indices[i]:indices[i]+counts[i]][:,1])
+        mean_y = np.mean(data[indices[i]:indices[i]+counts[i]][:,2])
+        mean_z = np.mean(data[indices[i]:indices[i]+counts[i]][:,3])
+        std_x = np.std(data[indices[i]:indices[i]+counts[i]][:,1])
+        std_y = np.std(data[indices[i]:indices[i]+counts[i]][:,2])
+        std_z = np.std(data[indices[i]:indices[i]+counts[i]][:,3])
+        print("Range {} and {}".format(indices[i], indices[i]+counts[i]))
+        print("{},{},{},{},{},{}".format(mean_x,std_x,mean_y,std_y,mean_z,std_z))
+        f.write("{},{},{},{},{},{}".format(mean_x,std_x,mean_y,std_y,mean_z, std_z))
+        f.write('\n')
+        
+    f.close()
+        
+if __name__ == "__main__":
+    
+    filename = "csv-data/G5NZCJ017647206-Kay-left-hand_wash-soap-2019-09-21-07-12-36.csv"
+    indices, counts = read_csv_file(filename)
+    features_file = "features.csv"
+    write_features(filename, indices, counts)
+    
